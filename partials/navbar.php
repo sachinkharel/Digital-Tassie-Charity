1dd1951b502e5f7ae5d0f43433c44c785dc295be
Sachin <sachink1630@gmail.com>
2024-05-14 20:21:09 +1000

    navbar link fix

--- partials/navbar.php
+++ partials/navbar.php
@@ -32,7 +32,7 @@ if (isset($_SESSION['user-id'])) {
   <!-- We differeniated the navbar in two parts because one will be used for public view and this one -> here will be used for LoggedIn view -->
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">DTC</a>
+      <a class="navbar-brand" href="<?= ROOT_URL ?>charityHome.php">DTC</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
